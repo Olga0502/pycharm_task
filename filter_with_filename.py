9526200f7497517fbab95bc5c929f3e94f3fa13b
Bornovolokova Olga <bornovolokovalelik653@yandex.ru>
2021-11-22 17:41:51 +0500

    filter_with_filename.py

---
+++ filter_with_filename.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import numpy as np
+
+def get_brightness(arr_pixels, pix_x, pix_y, size_moz):
+    sum_color = np.sum(arr_pixels[pix_x: pix_x + size_moz, pix_y: pix_y + size_moz])
+    return int(sum_color // 3 // size_moz ** 2)
+
+def set_color(arr_pixels, brightness, size_moz, pix_x, pix_y, step):
+    value_grey = int(brightness // step) * step
+    arr_pixels[pix_x: pix_x + size_moz, pix_y: pix_y + size_moz] = value_grey
+
+def grey_img(file_image, gradation, size_moz):
+    arr_pixels = np.array(img)
+    height = len(arr_pixels)
+    width = len(arr_pixels[1])
+    step = 255 // gradation
+    pix_y = 0
+    for y in range(0, height, size_moz):
+        pix_x = 0
+        for x in range(0, width, size_moz):
+             brightness = get_brightness(arr_pixels, pix_x, pix_y, size_moz)
+             set_color(arr_pixels, brightness, size_moz, pix_x, pix_y, step)
+    return arr_pixels
+
+img = Image.open('img2.jpg')
+res = Image.fromarray(grey_img('img2.jpg', 50, 10))
+res.save('yes.jpg')
